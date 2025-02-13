d0933e4e2057ecb8fe5476cef9f75d12f9ab6735
Suvam Samantray <suvam.workforce@gmail.com>
2025-02-13 13:01:47 +0530

    just practicing JS regularly

--- emp.js
+++ emp.js
@@ -19,18 +19,44 @@
 // }
 
 
-let userRole="viewer"
-switch (userRole)
-{
-    case "admin":
-        console.log("full access");
-        break;
-    case "editor":
-        console.log("edit access");
-        break;
-    case "user":
-        console.log("view access");
-        break;
-    default:
-        console.log("trial access");
-}
+// let userRole="viewer"
+// switch (userRole)
+// {
+//     case "admin":
+//         console.log("full access");
+//         break;
+//     case "editor":
+//         console.log("edit access");
+//         break;
+//     case "user":
+//         console.log("view access");
+//         break;
+//     default:
+//         console.log("trial access");
+// }
+
+
+// let day=3;
+// switch(day)
+// {
+//     case 0:
+//     case 6:
+//         console.log("Weekend !!!");
+//         break;
+//     case 1:
+//         console.log("Monday ://");
+//         break;
+//     case 2:
+//     case 3:
+//     case 4:
+//         console.log("Weekdays :||");
+//         break;
+//     case 5:
+//         console.log("Friday :)");
+// }
+
+
+// let age=20;
+// let message;
+// message= age>18 ? "can vote" : "laat maro saale ko";
+// console.log(message);
