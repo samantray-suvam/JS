78949177e4324d03a16276e03d01e0efe607101a
Suvam Samantray <suvam.workforce@gmail.com>
2025-03-13 13:01:22 +0530

    Forward with daily practice

--- Functions.js
+++ Functions.js
@@ -1,8 +1,6 @@
 //no input, no output
-
 // let a = 100;
 // let b = 200;
-
 // function add() {
 //     let c;
 //     c = a + b;
@@ -12,63 +10,75 @@
 
 
 //no input, output
-
 // let a = 100;
 // let b = 200;
-
 // function add() {
-//     let c;
-//     c = a + b;
+//     let c = a + b;
 //     return c;
 // }
-// let res = add();    //calls function
-// console.log(res);   //prints
+// // let res = add();    //calls function
+// console.log(add());   //prints
+
 
 //input, no output
-
 // function add(a, b) {
-//     let c;
-//     c = a + b;
+//     let c = a + b;
 //     console.log(c);
 // }
-// let x = 500;
-// let y  = 200;
-// add(x,y);
+// add(400, 200);
+
 
 //input, output
-
 // function add(a, b) {
 //     let c;
 //     c = a + b;
 //     return c;
 // }
-// let x = 800;
-// let y  = 200;
-// let res = add(x, y);
-// console.log(res);
+// console.log(add(800, 200));
 
 
 //grade check using function
+// function gradeCheck(studMarks, maxMarks)
+// {
+//     let studPercentage = (studMarks / maxMarks) * 100;
+//     let studGrade;
 
-function gradeCheck(studMarks, maxMarks)
-{
-    let studPercentage = (studMarks / maxMarks) * 100;
-    let studGrade;
+//     if (studPercentage >= 90)
+//     {
+//         studGrade = 'A';
+//     }
+//     else if(studPercentage >= 80){
+//         studGrade = 'B';
+//     }
+//     else if(studPercentage >= 60){
+//         studGrade='C'
+//     }
+//     else {
+//         studGrade='D'
+//     }
+//     console.log(studGrade);
+// }
 
-    if (studPercentage >= 90)
-    {
-        studGrade = 'A';
-    }
-    else if(studPercentage >= 80){
-        studGrade = 'B';
-    }
-    else if(studPercentage >= 60){
-        studGrade='C'
-    }
-    else {
-        studGrade='D'
-    }
-    console.log(studGrade);
-}
+// gradeCheck(348, 400);
 
-gradeCheck(348, 400);
+
+//Function Expression
+// let fun = function(a, b)
+// {
+//     return a * b;
+// }
+// console.log(fun(26, 4));
+
+
+//Arrow Function
+// let fun = (a, b) =>
+// {
+//     return a * b;
+// }
+// console.log(fun(45, 7));
+
+
+//IIFE - Immediately Invoked Function Expression
+(function () {
+    console.log('Hello World');
+})();
