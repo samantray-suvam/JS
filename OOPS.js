cb66e6ace81e8c078c3f812030a47f747b37b259
Suvam Samantray <suvam.workforce@gmail.com>
2025-08-14 12:21:31 +0530

    starting with objects

---
+++ OOPS.js
@@ -0,0 +1,69 @@
+// Example - 1
+
+// let a = {
+//     // state/ properties
+//     name: "Car",
+//     model: "Nissan",
+//     price: 82.77,
+
+//     // behaviour
+//     start : function () {
+//         console.log("Car started");
+//     },
+
+//     stop : function () {
+//         console.log("Car stopped");
+//     },
+
+//     accelerate: function () {
+//         console.log("Car accelerated");
+//     },
+
+//     getInfo: function () {
+//         return `Name: ${this.name}, Model: ${this.model}, Price: ${this.price}`;
+//     }
+// }
+
+// console.log(a.getInfo());
+// a.start();
+// a.stop();
+// a.accelerate();
+
+
+
+// Example - 2
+
+// let student = {
+//     name: "Raghu",
+//     age: 21,
+//     grade: "A",
+
+//     code: function () {
+//         console.log(`${this.name} can code`);
+//     },
+
+//     play : function(){
+//         console.log(`${this.name} can play guitar`);
+//     },
+
+//     create: function () {
+//         console.log(`${this.name} can create 3D models`);
+//     }
+// }
+
+// console.log(student.age);
+// console.log(student.grade);
+
+// student.code();
+// student.play();
+// student.create();
+
+
+
+let a = 100;
+var b;
+
+b = a;
+console.log(b);
+b = 200;
+console.log(b);
